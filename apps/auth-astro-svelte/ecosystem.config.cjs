2ace904dcde49f37aef6c9462fa07a2ed0297c4e
kerem <keremtiryaki@gmail.com>
2024-03-10 18:28:05 -0400

    Update ecosystem.config.cjs files and Caddyfile

--- apps/auth-astro-svelte/ecosystem.config.cjs
+++ apps/auth-astro-svelte/ecosystem.config.cjs
@@ -1,12 +1,12 @@
 module.exports = {
     apps: [
         {
-            name: "www-astro-svelte",
+            name: "auth-astro-svelte",
             script: "./dist/server/entry.mjs",
             // cron_restart: "0 */6 * * *", // every 6 hours
             env: {
                 "HOST": "0.0.0.0",
-                "PORT": "4333"
+                "PORT": "4322"
             }
         },
     ],
